23b35f5bb575f13bb2d3ea75ea43b82e441b9679
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-25 17:44:39 +0530

    RoundedRectangle shape added

--- SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -9,9 +9,35 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    var isYellow = true
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack() {
+                Text("Shapes")
+                Text("Shapes Introduction")
+                Text("Shapes with background color. Shapes with background color. Shapes with background color")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .font(.title2)
+                Text("RoundedRectangle is a common Shape")
+                    .foregroundColor(.white)
+                    .padding()
+                    .font(.title2)
+                    .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.red))
+                Text("Text has Corner Radius")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.red)
+                    .cornerRadius(20)
+            }
+            .font(.title)
+            .foregroundColor(.blue)
+            .navigationBarTitle("Shapes")
+        }
+        
     }
     
 }
---
+++ SwiftUIBasics/SwiftUIBasics/SignUpViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewModel.swift
+//  SwiftUIBasics
+//
+//  Created by Gurjinder Singh on 25/04/22.
+//
+
+import Foundation
+
+struct SignUpViewModel {
+    var firstName = ""
+    var lastName = ""
+    var userName = ""
+    var password = ""
+}
