50d053ae0244b3e3934c2f24358d08bbb7ee16c4
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-26 21:36:39 +0530

    VStack and HStack implemented

--- SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -11,13 +11,96 @@ import CoreData
 struct ContentView: View {
     var isYellow = true
     var body: some View {
-        Text("Hello World")
+        ScrollView() {
+
+        VStack(spacing: 30) {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Alignment Leading")
+                    Divider()
+                    Image(systemName: "arrow.left")
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.blue))
+                
+                VStack(alignment: .trailing, spacing: 20) {
+                    Text("Alignment Trailing")
+                    Divider()
+                    Image(systemName: "arrow.right")
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.blue))
+                
+                VStack () {
+                    Text("Centered Align by defalut")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.accentColor))
+                    
+                }
+                Divider()
+                HStack () {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                }
+                .padding()
+                .font(.title)
+                .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.teal))
+                Divider()
+                Text("Spacing 100")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                HStack (spacing: 100) {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                }
+                .padding()
+                .font(.title)
+                .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.teal))
+                Divider()
+                VStack () {
+                    HStack(alignment: .top
+                    ) {
+                        Rectangle().foregroundColor(.red).frame(width: 20,height: 100)
+                        Text("Leading").background(.red)
+                        Spacer()
+                        Text("Center").background(.red)
+                        Spacer()
+                        Text("Trailing").background(.red)
+                    }.border(.white,width: 3)
+                    HStack() {
+                        Rectangle().foregroundColor(.red).frame(width: 20,height: 100)
+                        Text("Leading").background(.red)
+                        Spacer()
+                        Text("Center").background(.red)
+                        Spacer()
+                        Text("Trailing").background(.red)
+                    }.border(.white,width: 3)
+                    
+                    HStack(alignment: .bottom) {
+                        Rectangle().foregroundColor(.red).frame(width: 20,height: 100)
+                        Text("Leading").background(.red)
+                        Spacer()
+                        Text("Center").background(.red)
+                        Spacer()
+                        Text("Trailing").background(.red)
+                    }.border(.white,width: 3)
+                    
+                   
+                }
+                
+            }.background(.black)
+        }
     }
     
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
